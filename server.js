79d681ce9b1b6150f7be6c453b8c35ac4f98d8a9
wesleytyber <wesleytyber@gmail.com>
2025-09-19 10:28:00 -0300

    [main] - adjusted css and icons

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
-import path from 'path';
 import tmi from 'tmi.js';
-import { fileURLToPath } from 'url';
 
 const app = express();
 const server = http.createServer(app);
